11e68abdc9c63129f17013f59ebaa71509842210
KelvinCorrea19 <kelvin.correa@hotmail.com>
2024-10-30 23:17:27 -0300

    Teste do Xp

---
+++ src/index.js
@@ -0,0 +1,67 @@
+let nomeDoHeroi = "Kelvin";
+let nivelDoHeroi = "999" ;
+let nivelFerro = "Ferro "
+let nivelPrata = " Prata"
+let nivelBronze = "Bronze "
+let nivelOuro = " Ouro "
+let nivelPlatina = " Platina "
+let nivelAscendente = " Ascendente "
+let nivelImortal = " Imortal "
+let nivelRadiante = " Radiante "
+
+while ( nivelDoHeroi <= 999 ) {
+    
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelFerro)
+    
+    break
+
+}
+
+ while ( nivelDoHeroi > 1001  <= 2000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelBronze)
+    
+    break
+
+}
+
+while ( nivelDoHeroi > 2001  <= 5000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelPrata)
+    
+    break
+
+}
+while ( nivelDoHeroi > 5001  <= 7000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelOuro)
+    
+    break
+
+}
+while ( nivelDoHeroi > 7001  <= 8000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelPlatina)
+    
+    break
+
+}
+
+while ( nivelDoHeroi > 8001  <= 9000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelAscendente)
+    
+    break
+
+}
+
+while ( nivelDoHeroi > 9001  <= 10000) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelImortal)
+    
+    break
+
+}
+
+while ( nivelDoHeroi > 10001) {
+    console.log ("O Héroi de nome " + nomeDoHeroi  + " está no nível de " + nivelRadiante)
+    
+    break
+
+}
+
+
